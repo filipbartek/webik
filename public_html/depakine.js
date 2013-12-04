ca0ebd5864b1771131c9c44c317ffb07c37c5e0f
Filip Bártek <filip.bartek@hotmail.com>
2013-12-04 05:28:22 +0100

    depakine*: Section expanding now works

--- public_html/depakine.js
+++ public_html/depakine.js
@@ -2,17 +2,28 @@
 
 function hide(event) {
   event.stopPropagation();
-  var header = event.currentTarget;
-  var parent = header.parentElement;
-  var tagName = parent.tagName;
-  if (tagName == "SECTION") {
-    var classList = parent.classList;
-    if (classList.contains("hidden")) {
-      classList.remove("hidden");
-    } else {
-      classList.add("hidden");
-    }
+  var button = event.currentTarget;
+  console.assert(button.tagName == "BUTTON");
+  var header = button.parentElement;
+  console.assert(header.tagName == "HEADER");
+  var section = header.parentElement;
+  console.assert(section.tagName == "SECTION");
+  var classList = section.classList;
+  if (classList.contains("expanded")) {
+    classList.remove("expanded");
+    button.textContent = "+";
+  } else {
+    classList.add("expanded");
+    button.textContent = "-";
   }
+}
+
+function addPlusButton(header) {
+  var plusButton = document.createElement("BUTTON");
+  var plusText = document.createTextNode("+");
+  plusButton.appendChild(plusText);
+  plusButton.addEventListener("click", hide);
+  header.insertBefore(plusButton, header.firstChild);
 }
 
 var sections = document.getElementsByTagName("section");
@@ -22,15 +33,9 @@ for (sectionIndex = 0; sectionIndex < sections.length; sectionIndex++) {
   for (sectionChildIndex = 0; sectionChildIndex < sectionChildren.length;
       sectionChildIndex++) {
     var sectionChild = sectionChildren[sectionChildIndex];
-    var tagName = sectionChild.tagName;
-    if (tagName == "HEADER") {
-      //console.log(sectionChild);
-      sectionChild.addEventListener("click", hide);
+    if (sectionChild.tagName == "HEADER") {
+      var header = sectionChild;
+      addPlusButton(header);
     }
   }
-  /*if (typeof section.childNodes === "undefined") {
-    console.log("yes");
-  } else {
-    console.log("no");
-  }*/
 }
